e8b71ac5a51f31278ea42ab74c4228a8269c69b6
Lyubov Zabavina <zabavinal@yandex.ru>
2021-03-13 22:01:27 +0300

    Tests added

---
+++ README.md
@@ -0,0 +1,1 @@
+## Project Status [![Build status](https://ci.appveyor.com/api/projects/status/r4mtrqmp9ajgg5kn/branch/master?svg=true)](https://ci.appveyor.com/project/ZabavinaL/selenide/branch/master)
---
+++ src/test/java/ru/netology/card/CardDeliveryTest.java
@@ -0,0 +1,103 @@
+package ru.netology.card;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.exactText;
+
+
+public class CardDeliveryTest {
+    LocalDate date = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    LocalDate nextDate = date.plusDays(3);
+
+
+    @Test
+    void shouldCorrectValues() {
+        open("http://0.0.0.0:7777");
+        $("[data-test-id = 'city'] input").setValue("Воронеж");
+        $("[placeholder = 'Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
+        $("[placeholder = 'Дата встречи']").setValue(formatter.format(nextDate));
+        $("[data-test-id = 'name'] input").setValue("Иванов Петр");
+        $("[name = 'phone']").setValue("+79777777777");
+        $("[data-test-id = 'agreement']").click();
+        $(Selectors.byText("Забронировать")).click();
+        $(Selectors.withText("Успешно!")).waitUntil(Condition.visible, 15000);
+    }
+
+    @Test
+    void IfIncorrectCityValue() {
+        open("http://0.0.0.0:7777");
+        $("[data-test-id = 'city'] input").setValue("Voronezh");
+        $("[placeholder = 'Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
+        $("[placeholder = 'Дата встречи']").setValue(formatter.format(nextDate));
+        $("[data-test-id = 'name'] input").setValue("Иванов Петр");
+        $("[name = 'phone']").setValue("+79777777777");
+        $("[data-test-id = 'agreement']").click();
+        $(Selectors.byText("Забронировать")).click();
+        $("[data-test-id = 'city'] .input__inner .input__sub")
+                .shouldHave(exactText("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void IfIncorrectNameValue() {
+        open("http://0.0.0.0:7777");
+        $("[data-test-id = 'city'] input").setValue("Воронеж");
+        $("[placeholder = 'Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
+        $("[placeholder = 'Дата встречи']").setValue(formatter.format(nextDate));
+        $("[data-test-id = 'name'] input").setValue("Ivan.");
+        $("[name = 'phone']").setValue("+79777777777");
+        $("[data-test-id = 'agreement']").click();
+        $(Selectors.byText("Забронировать")).click();
+        $("[data-test-id = 'name'] .input_invalid .input__sub")
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void IfIncorrectPhoneValue() {
+        open("http://0.0.0.0:7777");
+        $("[data-test-id = 'city'] input").setValue("Воронеж");
+        $("[placeholder = 'Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
+        $("[placeholder = 'Дата встречи']").setValue(formatter.format(nextDate));
+        $("[data-test-id = 'name'] input").setValue("Петр Иванович");
+        $("[name = 'phone']").setValue("телефон");
+        $("[data-test-id = 'agreement']").click();
+        $(Selectors.byText("Забронировать")).click();
+        $("[data-test-id = 'phone'] .input__inner .input__sub")
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void IfEmptyValueOfName() {
+        open("http://0.0.0.0:7777");
+        $("[data-test-id = 'city'] input").setValue("");
+        $("[placeholder = 'Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
+        $("[placeholder = 'Дата встречи']").setValue(formatter.format(nextDate));
+        $("[data-test-id = 'name'] input").setValue("Петр Иванович");
+        $("[name = 'phone']").setValue("+79777777777");
+        $("[data-test-id = 'agreement']").click();
+        $(Selectors.byText("Забронировать")).click();
+        $("[data-test-id = 'city'] .input__inner .input__sub")
+                .shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void IfNotClickCheckbox() {
+        open("http://0.0.0.0:7777");
+        $("[data-test-id = 'city'] input").setValue("Воронеж");
+        $("[placeholder = 'Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
+        $("[placeholder = 'Дата встречи']").setValue(formatter.format(nextDate));
+        $("[data-test-id = 'name'] input").setValue("Петр Иванович");
+        $("[name = 'phone']").setValue("+79777777777");
+        $(Selectors.byText("Забронировать")).click();
+        $("[data-test-id= 'agreement'] .checkbox__text")
+                .shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+    }
+}
